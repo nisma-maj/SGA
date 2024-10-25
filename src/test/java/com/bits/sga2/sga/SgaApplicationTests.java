1be725bf89081eab87eb708fde37ad43737a5540
nisma-maj <fnmajeed1998@gmail.com>
2024-10-25 16:19:24 +0200

    Inital Config

---
+++ src/test/java/com/bits/sga2/sga/SgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.sga2.sga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
